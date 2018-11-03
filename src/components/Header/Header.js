7f401827e044bcb3a2066e56061f9105d44afe88
Clayton Hart <claytonhart123@yahoo.com>
2018-11-03 15:12:17 -0700

    Updated Header to allow login in / out

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,87 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { logoutUser } from "../../store/actions/authActions";
 
 import "./Header.css";
 
-const Header = props => {
-  return (
-    <nav className="nav-container">
-      <div className="header">
-        <h2 className="logo">
-          <Link to="/">Playlist Converter</Link>
-        </h2>
-        <ul className="main-nav">
-          <li>
-            <Link to="/playlists">Top Playlists</Link>
-          </li>
-          <li className="inverted">
-            <Link to="/convert">Convert a playlist</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-          <li>
-            <Link to="/login">Log In</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
+class Header extends Component {
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
 
-export default Header;
+  render() {
+    const { isAuthenticated } = this.props.auth;
+
+    const authLinks = (
+      <ul className="main-nav">
+        <li>
+          <Link to="/playlists">Top Playlists</Link>
+        </li>
+        <li className="inverted">
+          <Link to="/convert">Convert a playlist</Link>
+        </li>
+        <li>
+          <Link to="/my-playlists">Your Playlists</Link>
+        </li>
+        <li>
+          <a href="/" onClick={this.onLogoutClick}>
+            Logout
+          </a>
+        </li>
+      </ul>
+    );
+
+    const guestLinks = (
+      <ul className="main-nav">
+        <li>
+          <Link to="/playlists">Top Playlists</Link>
+        </li>
+        <li className="inverted">
+          <Link to="/convert">Convert a playlist</Link>
+        </li>
+        <li>
+          <Link to="/signup">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/login">Log In</Link>
+        </li>
+      </ul>
+    );
+
+    return (
+      <nav className="nav-container">
+        <div className="header">
+          <h2 className="logo">
+            <Link to="/">Playlist Converter</Link>
+          </h2>
+          {isAuthenticated ? authLinks : guestLinks}
+          {/* <ul className="main-nav">
+            <li>
+              <Link to="/playlists">Top Playlists</Link>
+            </li>
+            <li className="inverted">
+              <Link to="/convert">Convert a playlist</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+            <li>
+              <Link to="/login">Log In</Link>
+            </li>
+          </ul> */}
+        </div>
+      </nav>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Header);
